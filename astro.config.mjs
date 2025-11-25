7ea3b6bf5c535a637079cf337eb75e4065603030
nuonuo-888 <2720161028@qq.com>
2025-11-25 17:12:37 +0800

    chore: upgrade astro and edgeone adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
+  adapter: edgeoneAdapter(),
+  viewTransitions: true,
   integrations: [tailwind(), compress()],
 });
